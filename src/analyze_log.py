f8c02c16e8985a6fab22564dec03b4913a2a0573
HenriqueSerres <henriqueserres@gmail.com>
2022-10-28 14:41:37 -0300

    fix lint

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -9,12 +9,13 @@ def analyze_log(path_to_file):
     try:
         with open(path_to_file, encoding="utf8") as file:
             content = csv.reader(file, delimiter=",", quotechar='"')
-            for customer, order, day in content:
-                trackOrders.add_new_order(customer, order, day)
-            most_ordered = trackOrders.get_most_ordered_dish_per_customer("maria")
-            frequency = trackOrders.get_how_many_times("arnaldo", "hamburguer")
-            never_ordered = trackOrders.get_never_ordered_per_customer("joao")
-            never_visited = trackOrders.get_days_never_visited_per_customer("joao")
+            print("XXXX", content)
+        for customer, order, day in content:
+            trackOrders.add_new_order(customer, order, day)
+        most_ordered = trackOrders.get_most_ordered_dish_per_customer("maria")
+        frequency = trackOrders.get_how_many_times("arnaldo", "hamburguer")
+        never_ordered = trackOrders.get_never_ordered_per_customer("joao")
+        never_visited = trackOrders.get_days_never_visited_per_customer("joao")
 
         with open("data/mkt_campaign.txt", "w") as file:
             file.write(
